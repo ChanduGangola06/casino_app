23e642f5f1239b9aaab45005ff8b8afd08eb2bb6
Chandu Gangola <chandugangola444@gmail.com>
2024-05-13 22:59:44 +0530

    initialize the app

---
+++ android/app/src/main/kotlin/com/admin/casino_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.admin.casino_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/utils/my_icons.dart
@@ -0,0 +1,4 @@
+class MyIcons {
+  static const String verifiedIcon = "assets/images/verified-user.svg";
+  static const String pendingIcon = "assets/images/pending_icon.svg";
+}
